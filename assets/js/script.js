0c7a6f0fc4edc3b5c971c9ef1701d51043ee19a3
Antonio Quistelli <antonio.quistelli@gmail.com>
2024-04-16 19:41:25 +0200

    new menu style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Seleziona il pulsante del menù a hamburger
     const hamburger = document.querySelector('.menu-hamburger');
-    // Seleziona il menù a tendina
+    // Seleziona il menù
     const menu = document.querySelector('.menu-hidden');
 
     // Funzione per aprire/chiudere il menù
     function toggleMenu(event) {
+        // Alterna la visibilità del menù
         menu.classList.toggle('visibile');
         event.stopPropagation(); // Impedisce che l'evento si propaghi al document
     }
