9315460aa27718f1aa3d45657e35c142f6e80c2c
Antonio Quistelli <antonio.quistelli@gmail.com>
2024-04-17 11:49:04 +0200

    modified who i am and home to fix some stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Seleziona il pulsante del menù a hamburger
     const hamburger = document.querySelector('.menu-hamburger');
-    // Seleziona il menù
     const menu = document.querySelector('.menu-hidden');
 
-    // Funzione per aprire/chiudere il menù
-    function toggleMenu(event) {
-        // Alterna la visibilità del menù
+    hamburger.addEventListener('click', function() {
+        // Toggle visibility of the menu
         menu.classList.toggle('visibile');
-        event.stopPropagation(); // Impedisce che l'evento si propaghi al document
-    }
+    });
 
-    // Gestore di eventi per il clic sul menù a hamburger
-    hamburger.addEventListener('click', toggleMenu);
-
-    // Gestore di eventi per il clic sul document
+    // Optional: Close the menu when clicking outside of it
     document.addEventListener('click', function(event) {
-        // Se il menù è visibile e il clic non è sul menù o sul pulsante del menù
-        if (menu.classList.contains('visibile') && !menu.contains(event.target) && !hamburger.contains(event.target)) {
-            menu.classList.remove('visibile'); // Nasconde il menù
+        if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
+            menu.classList.remove('visibile');
         }
     });
 
-    // Prevenire la chiusura del menù quando si clicca al suo interno
+    // Prevent closing menu when clicking inside
     menu.addEventListener('click', function(event) {
         event.stopPropagation();
     });
